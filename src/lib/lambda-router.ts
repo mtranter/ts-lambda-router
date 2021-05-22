3df49f51b93038878253f2df93a1666b0174669a
Mark Tranter <mark.a.tranter@gmail.com>
2021-05-23 09:58:38 +1000

    Extend logging

--- src/lib/lambda-router.ts
+++ src/lib/lambda-router.ts
@@ -1,12 +1,14 @@
+import { Logger } from "./types";
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { APIEventHandler } from "./handler";
 import { Router } from "./router";
 
 export const LambdaRouter = {
   build: (
-    routes: <R>(router: Router<R>) => Router<R>
+    routes: <R>(router: Router<R>) => Router<R>,
+    logger?: Logger | undefined
   ): APIGatewayProxyHandler => {
     const routeDef = routes(Router());
-    return APIEventHandler(routeDef);
+    return APIEventHandler(routeDef, logger);
   },
 };
