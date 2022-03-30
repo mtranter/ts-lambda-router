3e3fc6b2aa85d8f261c5d51760c989f510f4c24e
Mark Tranter <mark.a.tranter@gmail.com>
2022-03-30 22:19:57 +1100

    Use require statements

--- src/lib/local.ts
+++ src/lib/local.ts
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
-import path from "path";
-import http from "http";
-import querystring from "querystring";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-console.log(process.argv);
+const path = require("path");
+const http = require("http");
+const querystring = require("querystring");
+
 const cwd = process.cwd();
 const file = process.argv[2];
 const handler = process.argv[3];
@@ -15,38 +14,40 @@ const handlerModule = require(path.join(cwd, file));
 const handlerFn = handlerModule[handler];
 
 if (typeof handlerFn === "function") {
-  const apiHandler: (ev: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult> = handlerFn;
-  const server = http.createServer(async (req, res) => {
+  const apiHandler = handlerFn;
+  const server = http.createServer(async (req: any, res: any) => {
     const query = req.url?.includes("?")
       ? querystring.parse(req.url!.split("?")[1])
       : null;
     let body = "";
-    req.on("data", (c) => {
+    req.on("data", (c: any) => {
       body += c;
     });
     req.on("end", async () => {
-      const response = await apiHandler(
-        {
-          path: req.url!.split("?")[0],
-          httpMethod: req.method!,
-          body,
-          headers: req.headers as any,
-          queryStringParameters: query,
-        } as any
-      ).catch((e) => {
-          console.error(e)
-          return {
-              statusCode: 500,
-              body: e.toString()
-          }
+      const response = await apiHandler({
+        path: req.url!.split("?")[0],
+        httpMethod: req.method!,
+        body,
+        headers: req.headers as any,
+        queryStringParameters: query,
+      } as any).catch((e: any) => {
+        console.error(e);
+        return {
+          statusCode: 500,
+          body: e.toString(),
+        };
       });
       res.writeHead(response.statusCode);
       res.end(response.body);
     });
   });
   server.listen(port, "127.0.0.1", () => {
-    console.log(`ts-lambda-router-local Server is running on http://127.0.0.1:${port}`);
+    console.log(
+      `ts-lambda-router-local Server is running on http://127.0.0.1:${port}`
+    );
   });
 } else {
-  console.log("Usage: ts-lambda-router-local <handler file> <handler function> [<port>]");
+  console.log(
+    "Usage: ts-lambda-router-local <handler file> <handler function> [<port>]"
+  );
 }
